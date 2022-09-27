be2e0177b10040024c8b6f655eb475f3c48c60a4
keito4 <newton30000@gmail.com>
2022-09-27 21:17:29 +0900

    reflect macos changing

---
+++ .zsh/configs/virtual/java.zsh
@@ -0,0 +1,4 @@
+export JAVA_HOME="$HOME/.asdf/installs/java/zulu-8.56.0.23/zulu-8.jdk/Contents/Home"
+
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="${PATH}:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools"
