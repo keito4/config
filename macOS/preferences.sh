de4cd9226b525701e4e2f2fac6f37c5479991288
keito4 <newton30000@gmail.com>
2022-09-29 16:56:05 +0900

    add 1password

--- macOS/preferences.sh
+++ macOS/preferences.sh
@@ -1,6 +1,7 @@
 # com.apple.dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock largesize -float 100
+defaults write com.apple.finder AppleShowAllFiles -boolean true
 
 # TODO: Add other preferences here
 
