846be1befe45cc4f346057d20b2f3ce56cfadcb3
keito4 <newton30000@gmail.com>
2023-07-28 01:44:24 +0900

    Add environment variables, functions, and Homebrew packages, as well as VS Code extensions and Git configurations.

---
+++ .zsh/configs/pre/env.zsh
@@ -0,0 +1,4 @@
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
