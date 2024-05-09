1def68b97f74e7be92dbd5202007e4b6a4e6e44a
keito4 <newton30000@gmail.com>
2024-05-09 10:33:18 +0900

    remove cargo

--- .zsh/configs/pre/path.zsh
+++ .zsh/configs/pre/path.zsh
@@ -5,9 +5,6 @@ PATH="/opt/homebrew/bin:$PATH"
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# Try loading ASDF from the regular home dir location
-PATH="$HOME/.cargo/bin:$PATH"
-
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
