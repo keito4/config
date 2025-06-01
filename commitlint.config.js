9ad55a112ba7682d2211b5339099d672a0391d09
keito4 <newton30000@gmail.com>
2025-06-01 12:34:07 +0900

    chore: commitlint設定を更新し、subject-caseルールを無効化。Huskyのpre-commitスクリプトを修正。

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,6 +19,7 @@ module.exports = {
       ]
     ],
     'subject-max-length': [2, 'always', 72],
-    'header-max-length': [2, 'always', 72]
+    'header-max-length': [2, 'always', 72],
+    'subject-case': [0]
   }
 };
