ebc7b22da1bca24e1a3d7f8257eaa9c1e20e2c04
keito4 <newton30000@gmail.com>
2026-06-22 13:16:41 +0900

    fix: support caps lock ime shortcuts in cmux

--- test/nix-darwin-config.test.js
+++ test/nix-darwin-config.test.js
@@ -82,6 +82,9 @@ describe('nix-darwin and home-manager macOS configuration', () => {
     expect(karabinerModule).toContain('cmuxImeShortcut "j" "japanese_kana"');
     expect(karabinerModule).toContain('cmuxImeShortcut "semicolon" "japanese_eisuu"');
     expect(karabinerModule).toContain('cmuxImeShortcut "quote" "japanese_eisuu"');
+    expect(karabinerModule).toContain('cmuxCapsLockImeShortcut "j" "japanese_kana"');
+    expect(karabinerModule).toContain('cmuxCapsLockImeShortcut "semicolon" "japanese_eisuu"');
+    expect(karabinerModule).toContain('cmuxCapsLockImeShortcut "quote" "japanese_eisuu"');
   });
 
   test('portable user dotfiles are managed without credential state', () => {
